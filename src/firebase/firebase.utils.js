e7cdfaea2e7c264b7b6f47e45851136d52bcc7cc
Alex Karofilakis <akarofylakis@gmail.com>
2020-07-21 11:40:52 +0300

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyBB70iHHo8OwxFKEuJ7lhEdQQx9IQx5l1M',
-  authDomain: 'staysharp-251cb.firebaseapp.com',
-  databaseURL: 'https://staysharp-251cb.firebaseio.com',
-  projectId: 'staysharp-251cb',
-  storageBucket: 'staysharp-251cb.appspot.com',
-  messagingSenderId: '222160648858',
-  appId: '1:222160648858:web:9183ab0193f9722a3cbd23',
-  measurementId: 'G-1MH4VLHX14',
+
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
